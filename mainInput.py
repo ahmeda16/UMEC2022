167016d2e784091cb0ed5d9cfe047bbedf005b3c
Syed Abraham Ahmed <ahmeda16@myumanitoba.ca>
2022-10-15 12:02:21 -0500

    Initiated Python File For Parsing
    mainInput for main project (master)

---
+++ mainInput.py
@@ -0,0 +1,17 @@
+import lib.pymongo
+
+class mainProject:
+
+    def __init__ (self, mainName, description):
+        self.mainName = mainName
+        self.description = description 
+
+    def getMainName(self):
+        return self.mainName
+
+    def getDescription(self):
+        return self.getDescription
+
+
+
+
