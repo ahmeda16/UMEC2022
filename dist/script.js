3932d9ff0474b8d8624797c570d5c988a1d143a0
Backback12 <cepagtakhan@gmail.com>
2022-10-15 13:26:45 -0500

    Update Request Functions

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,37 @@
 "use strict";
 
+const MONGO_ID = "634af07248a321b8c5475e7f"
+const MONGO_KEY = "bfFfyFERzLuUKXzGofdSRqBaODyOARolgQjyveuTo5EuhIerYL5UknSwAG3ZLMGo"
+
+function getMongo() {
+    let action = "/action/findOne"
+    let baseUrl = `https://data.mongodb-api.com/app/${MONGO_ID}/endpoint/data/v1${action}`
+
+    $.ajax("https://api.spotify.com/v1" + baseUrl, {
+        dataType: 'json',
+        type: 'POST',
+        data: {
+            'dataSource': 'input',
+            'database': 'masterDB',
+            'collection': 'test project',
+            'document': {
+                "name": "John Sample",
+                "age": 69,
+            }
+        },
+        headers: {
+            //'Authorization': 'Bearer ' + accessToken
+            'Access-Control-Allow_Headers': '*',
+            'Content-Type': 'application/json',
+            'api-key': MONGO_KEY,
+        },
+        success: function(r) {
+            console.log("Request Worked " + JSON.stringify(r));
+            // resolve(r);
+        },
+        error: function(err) {
+            console.log("Request Failed " + JSON.stringify(err));
+            // reject(err);
+        }
+    })
+}
--- mainInput.py
+++ mainInput.py
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 from lib.pymongo import MongoClient
 
+=======
+# import pymongo
+# from pymongo import MongoClient
+# from lib.pymongo.main import MongoClient
+from pyodide.http import pyfetch
+import asyncio
+
+MONGO_ID = "634af07248a321b8c5475e7f"
+MONGO_KEY = "bfFfyFERzLuUKXzGofdSRqBaODyOARolgQjyveuTo5EuhIerYL5UknSwAG3ZLMGo"
+MONGO_CLUSTER = "input"
+>>>>>>> Stashed changes
 
 class mainProject:
 
@@ -11,5 +23,44 @@ class mainProject:
         return self.mainName
 
     def getDescription(self):
+<<<<<<< Updated upstream
         return self.description
         
+=======
+        return self.getDescription
+
+
+from js import XMLHttpRequest
+async def getSpotify():
+    url = "https://jsonplaceholder.typicode.com/todos/1" #"https://www.adafruit.com/api/quotes.php"
+    # response = await pyfetch(url=url, method="GET")
+    # pyscript.write(f"status: {response.status}, json: {await response.json()}")
+
+    req = XMLHttpRequest.new()
+    req.open("GET", url, False)
+    req.send(None)
+    print(str(req.response))
+
+async def getMongo():
+    action = "/action/findOne"
+    baseUrl = f"https://data.mongodb-api.com/app/{MONGO_ID}/endpoint/data/v1" + action
+
+    req = XMLHttpRequest.new()
+    req.open("POST", baseUrl, False)
+    req.setRequestHeader("Content-Type", "application/json")
+    req.setRequestHeader("Access-Control-Allow_Origin", "False")
+    req.setRequestHeader("api-key", MONGO_KEY)
+    body = {
+        "dataSource": MONGO_CLUSTER,
+        "database": "masterDB",
+        "collection": "test project",
+        #"filter": {"name": "John Sample"}
+    }
+    req.send(body)
+    print(str(req.response))
+
+def printme():
+    print("WOWW")
+
+
+>>>>>>> Stashed changes
